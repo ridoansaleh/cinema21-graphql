43d3256060f822a870c5fac72fc999e6ca651f06
RSN <ridoansalehnasution98@gmail.com>
2019-09-15 12:59:31 +0700

    initialize version 2 (#2)

--- connection.js
+++ connection.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql");
+require("dotenv").config();
 
 const dbConfig = mysql.createConnection({
-  host: "localhost",
-  user: "ridoan",
-  password: "mantap",
-  database: "cinema21"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 const connectToDatabase = () => {
